e646581ae6746cd01c1954e2d91ba203afec61b0
VIVEK SINGH <114601400+singhcoder694@users.noreply.github.com>
2024-02-11 15:02:48 +0530

    Update Matrix.jsx

--- src/Matrix_Calculator/Components/Matrix.jsx
+++ src/Matrix_Calculator/Components/Matrix.jsx
@@ -24,7 +24,7 @@ function Matrix(props){
         const newJoke = {
             text:inputValue,
         };
-        const response= await fetch(`http://localhost:8000/${e.target.value.slice(-3)}`, {
+        const response= await fetch(`https://matrix-calculator-backend.onrender.com/${e.target.value.slice(-3)}`, {
             method:"POST",
             headers: { 
                 "Content-Type": "application/json",
